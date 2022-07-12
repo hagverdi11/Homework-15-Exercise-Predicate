32ff1428d1cfc7565111e9df1aebc1fff818c241
hagverdi11 <hagverdiam@code.edu.az>
2022-07-13 01:45:13 +0400

    Added new homework table

---
+++ Homework 15 Delegate Exercise/Homework 15 Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_15_Delegate
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Haqverdi");
+            names.Add("Akif");
+            names.Add("Akif");
+            names.Add("Akif");
+            names.Add("Akif");
+
+            CheckWords(m => m == "Akif", names);
+           
+        }
+
+        public static void CheckWords(Predicate<string> func, List<string>words)
+        {
+            var result = words.FindAll(func);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+                        
+        }
+
+        
+       
+    }
+}
